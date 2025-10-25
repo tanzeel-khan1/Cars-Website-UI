c129ecadc5729e0ba7c87d1514cdac6ae315dd67
tanzeel-khan1 <tanzeel0680@gmail.com>
2025-10-26 00:13:12 +0500

    work navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 import React, { useState } from "react";
 import { Menu, X } from "lucide-react";
 
@@ -7,11 +7,12 @@ const Navbar = () => {
 
   return (
     <>
+      
       <nav
         className="fixed top-0 left-0 z-50 h-14 w-full flex justify-between items-center 
         px-8 md:px-20 bg-black backdrop-blur-md shadow-lg transition-all duration-300"
       >
-        <h1 className="text-2xl font-bold text-white cursor-pointer hover:text-gray-300 transition pl-2 md:pl-16">
+        <h1 className="text-2xl font-bold text-white cursor-pointer hover:text-gray-300 transition pl-4 md:pl-16">
           LuxureDetails
         </h1>
 
@@ -22,7 +23,8 @@ const Navbar = () => {
               className="cursor-pointer relative group transition-all duration-300"
             >
               {item}
-              <span className="absolute left-0 bottom-[-3px] w-0 h-[2px] bg-white transition-all duration-300 group-hover:w-full"></span>
+              <span className="absolute left-0 bottom-[-3px] w-0 h-0.5 bg-white transition-all duration-300 group-hover:w-full"></span>
+
             </li>
           ))}
         </ul>
