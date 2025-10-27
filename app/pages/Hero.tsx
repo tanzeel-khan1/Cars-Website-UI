58ce2f6ed160ddd129ef5f38b6c30d42e8ff05be
tanzeel-khan1 <tanzeel0680@gmail.com>
2025-10-27 13:14:01 +0500

    add fix

--- app/pages/Hero.tsx
+++ app/pages/Hero.tsx
@@ -53,7 +53,7 @@ const Hero = () => {
         alt="Car"
         initial={{ opacity: 0, y: 80 }}
         whileInView={{ opacity: 1, y: 0 }}
-        transition={{ duration: 1.5, delay: 1, ease: "easeOut" }}
+        transition={{ duration: 1.5, ease: "easeOut" }}
         viewport={{ once: false, amount: 0.3 }}
       />
     </div>
